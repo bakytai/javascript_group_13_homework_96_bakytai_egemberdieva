4132d79d18f960845608bb817100cf212f5467a0
Bakytai Egemberdieva <bakijw@gmail.com>
2022-04-08 16:14:47 +0600

    создала модель пользователя и коктеля

---
+++ api/config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const rootPath = __dirname;
+
+module.exports = {
+    rootPath,
+    uploadPath: path.join(rootPath, 'public/uploads'),
+    mongo: {
+        db: 'mongodb://localhost/cocktailMenu',
+        options: {useNewUrlParser: true},
+    },
+    facebook: {
+        appId: '718422199316941',
+        appSecret: 'e822dc5e22ab51742e7d8b8ec4d8b6f9'
+    }
+};
---
+++ api/models/Cocktail.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const mime = require('mime-types');
+const config = require('../config');
+
+const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+
+const Ingredient = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+})
+
+const CocktailSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        validate: {
+            validator: function(value) {
+                const filePath = path.join(config.uploadPath, value);
+
+                const mimeType = mime.lookup(filePath);
+
+                return imageMimeTypes.includes(mimeType);
+            },
+            message: 'Image file format is incorrect'
+        }
+    },
+    recipe: {
+        type: String,
+        required: true
+    },
+    is_published: false,
+    ingredients: [
+        Ingredient
+    ]
+});
+
+const Cocktail = mongoose.model('Cocktail', CocktailSchema);
+
+module.exports = Cocktail;
